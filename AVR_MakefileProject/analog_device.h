2159ec5c8ae31cbf26c4d4ab76e71ddb9bf75fda
Stephen Kang <ki6080@gmail.com>
2019-05-19 17:29:49 +0900

    Merge global variable declarations onto one place

--- AVR_MakefileProject/analog_device.h
+++ AVR_MakefileProject/analog_device.h
@@ -1,9 +1,5 @@
 #pragma once
 #include "core.h"
 
-void InitializeAnalogDevice();
-
-extern byte ACC_MAX_INTERVAL;
-extern byte ACC_PERCENTX;
-extern byte ACC_PERCENTY;
+void InitializeAnalogDevice(); 
 void UpdateAccel();
