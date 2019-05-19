b4ee75ced93640a1f3d0cd41cbc825fcda9d5ed2
Stephen Kang <ki6080@gmail.com>
2019-05-19 18:43:17 +0900

    Parameterize interpolation method

--- AVR_MakefileProject/analog_device.c
+++ AVR_MakefileProject/analog_device.c
@@ -12,8 +12,13 @@ byte ACC_MAX_INTERVAL = 100;
 static byte ACC_INTERVAL_CNT = 0;
 static byte ACC_XCNT = 0;
 static byte ACC_YCNT = 0;
-static byte ACC_XARR[8];
-static byte ACC_YARR[8];
+
+#define SAMPLE_BITS 4
+#define SAMPLE_POW2 (1 << SAMPLE_BITS)
+#define SAMPLE_MOD (SAMPLE_POW2 - 1)
+
+static byte ACC_XARR[SAMPLE_POW2];
+static byte ACC_YARR[SAMPLE_POW2];
 static byte ACC_IDX = 0;
 void UpdateAccel()
 {
@@ -21,29 +26,17 @@ void UpdateAccel()
     if ( ACC_INTERVAL_CNT >= ACC_MAX_INTERVAL ) {
         ACC_INTERVAL_CNT = 0;
 
-        ACC_XARR[ACC_IDX++ & 7] = ACC_XCNT;// (uint16) ACC_XCNT * 300 / ACC_MAX_INTERVAL;
-        ACC_YARR[ACC_IDX++ & 7] = ACC_YCNT;// (uint16) ACC_YCNT * 300 / ACC_MAX_INTERVAL;
+        ACC_XARR[ACC_IDX++ & SAMPLE_MOD] = ACC_XCNT;// (uint16) ACC_XCNT * 300 / ACC_MAX_INTERVAL;
+        ACC_YARR[ACC_IDX++ & SAMPLE_MOD] = ACC_YCNT;// (uint16) ACC_YCNT * 300 / ACC_MAX_INTERVAL;
 
-        uint16 X = (
-            ACC_XARR[0] +
-            ACC_XARR[1] +
-            ACC_XARR[2] +
-            ACC_XARR[3] +
-            ACC_XARR[4] +
-            ACC_XARR[5] +
-            ACC_XARR[6] +
-            ACC_XARR[7] ) >> 3;
-
-        uint16 Y = (
-            ACC_YARR[0] +
-            ACC_YARR[1] +
-            ACC_YARR[2] +
-            ACC_YARR[3] +
-            ACC_YARR[4] +
-            ACC_YARR[5] +
-            ACC_YARR[6] +
-            ACC_YARR[7] ) >> 3;
-
+        uint16 X = 0, Y = 0;
+        byte i = SAMPLE_POW2;
+        while ( i-- ) {
+            X += ACC_XARR[i];
+            Y += ACC_YARR[i];
+        }
+        X >>= SAMPLE_BITS;
+        Y >>= SAMPLE_BITS;
         ACC_PERCENTX = (uint16) X * 300 / ACC_MAX_INTERVAL;
         ACC_PERCENTY = (uint16) Y * 300 / ACC_MAX_INTERVAL;
         ACC_XCNT = 0;
